ed40cc9229da38cf20b4e436968a5a27dfcd7a27
Aondre <aondref@gmail.com>
2021-07-25 17:22:16 -0400

    Finalized randomness

--- Version02/JS/scripts.js
+++ Version02/JS/scripts.js
@@ -1,5 +1,15 @@
+//The Great Randomization
 const animals = ["Lion", "Giraffe", "Fish", "Bird", "Elephant", "Monkey"];
+let emptyArray = []
 
-for (let j = 0; j < animals.length; j++) {
-    console.log(animals[j]);
+for(let j = 0; j < animals.length; j++) {
+    const randomNumber = Math.floor(Math.random() * animals.length);
+    if(emptyArray.length < animals.length) {
+
+        if(!emptyArray.includes(randomNumber)) {
+            emptyArray.push(randomNumber);
+        }
+        j = 0;
+    }
 }
+console.log(emptyArray);
