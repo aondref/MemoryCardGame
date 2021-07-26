efe990e07d3d214b474a53b9c475d991e1af0c2f
Aondre <aondref@gmail.com>
2021-07-25 20:01:58 -0400

    working on the start page

--- Version02/JS/scripts.js
+++ Version02/JS/scripts.js
@@ -1,15 +1,21 @@
-//The Great Randomization
-const animals = ["Lion", "Giraffe", "Fish", "Bird", "Elephant", "Monkey"];
-let emptyArray = []
+// //The Great Randomization
+// const animals = ["Lion", "Giraffe", "Fish", "Bird", "Elephant", "Monkey"];
+// let emptyArray = []
 
-for(let j = 0; j < animals.length; j++) {
-    const randomNumber = Math.floor(Math.random() * animals.length);
-    if(emptyArray.length < animals.length) {
+// //For Loop creates a randomized index number
+// for(let j = 0; j < animals.length; j++) {
+//     const randomNumber = Math.floor(Math.random() * animals.length);
+//     if(emptyArray.length < animals.length) {
 
-        if(!emptyArray.includes(randomNumber)) {
-            emptyArray.push(randomNumber);
-        }
-        j = 0;
-    }
-}
-console.log(emptyArray);
+//         if(!emptyArray.includes(randomNumber)) {
+//             emptyArray.push(randomNumber);
+//         }
+//         j = 0;
+//     }
+// }
+
+// //For Loop takes the animal array, using the random number as the index
+// for(let j = 0; j < animals.length; j++) {
+//     console.log(animals[emptyArray[j]]);
+// }
+
