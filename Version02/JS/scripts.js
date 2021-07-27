c6e02aa8f96a3e1fd5de45dea37b589229452ca6
Aondre <aondref@gmail.com>
2021-07-26 21:15:14 -0400

    I broke something. Yay!

--- Version02/JS/scripts.js
+++ Version02/JS/scripts.js
@@ -19,26 +19,52 @@
 //     console.log(animals[emptyArray[j]]);
 // }
 
-//Start Page Buttons
+//Buttons
 const startBtn = document.querySelector('#start-btn');
 const optionBtn = document.querySelector('#options-btn');
 const backBtn = document.querySelector('#back-btn');
+const continueBtn = document.querySelector('#continue-btn');
+const exitBtn = document.querySelector('#exit-btn');
 
-//Start and Player Pages
+//Start, Player, and Game Pages
 const buttons = document.querySelector('.buttons');
 const playerChoice = document.querySelector('#player-choice');
+const gamePage = document.querySelector('#game-page');
+const player01Choice = document.querySelector('#player01Btn');
+const player02Choice = document.querySelector('#player02Btn');
+const startGamePage = document.querySelector('#start-game-page');
+
+startGamePage.display.style = "grid";
 
 //Button Functionality
 startBtn.addEventListener("click", playerPage);
 backBtn.addEventListener("click", homePage);
+continueBtn.addEventListener("click", gameStart);
+exitBtn.addEventListener("click", exitButton);
+
 
 //Button Functions
 function playerPage() {
-    buttons.style.display = "none";
     playerChoice.style.display = "grid";
+    buttons.style.display = "none";
+    gamePage.style.display = "none";
 }
 
 function homePage() {
     buttons.style.display = "grid";
     playerChoice.style.display = "none";
+    gamePage.style.display = "none";
 }
+
+function gameStart() {
+    gamePage.style.display = "grid";
+    buttons.style.display = "none";
+    playerChoice.style.display = "none";
+}
+
+function exitButton() {
+    gamePage.display.style = "block";
+    startGamePage.display.style = "none";
+}
+
+//Game Initializer
